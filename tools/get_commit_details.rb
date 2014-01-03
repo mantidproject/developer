310c8c9ad1739fd5e15a3dae8e0e1668979f7556
Peter Peterson <petersonpf@ornl.gov>
2014-01-03 10:32:38 -0500

    Adding in a command line parser.

--- tools/get_commit_details.rb
+++ tools/get_commit_details.rb
@@ -1,26 +1,66 @@
 #!/usr/bin/env ruby
 require "date"
 require "net/http"
+require "optparse"
 
-# print usage information and exit
-def help val
-  puts "Usage: #{__FILE__} <git_repo_dir> [outfile]"
-  exit val
+# Find the Saturday before the date specified
+def prevSaturday date
+  if date.cwday == 6 # Saturday
+    return date
+  elsif date.cwday == 7 # Sunday
+    date -= 1
+  else # everything else
+    date -= (date.cwday+1)
+  end
 end
+
+# hash/map/dict of options
+options = {}
+
+# configure the options
+optparse = OptionParser.new do|opts|
+  opts.banner = "Usage: #{__FILE__} [options] <git_repo_dir>"
+
+  # specify the start date
+  options[:start] = prevSaturday(Date.today)
+  opts.on('', '--start DATE',
+          "Start date for git log messages (default to most recent Saturday past)") do |date|
+    options[:start] = prevSaturday(Date.parse(date))
+  end
+
+  # specify a file that already has tickets
+  options[:weekfile] = ""
+  opts.on('', '--file FILE',
+          "File with tickets already in it.") do |file|
+    options[:weekfile] = file
+  end
+
+  # displays the help
+  opts.on( '-h', '--help', 'Display the help' ) do
+    puts opts
+    exit
+  end
+end
+
+# parse and strip out the names of things
+optparse.parse!
 
 # parse the command line
 if ARGV.empty?
-  help false
-end
-mantid_src, filename = ARGV
-if mantid_src.empty?
   puts "Failed to supply git repository directory"
-  help false
+  puts optparse.help()
+  exit
 end
-date_start = Date.parse("2013-12-21")
-date_stop  = Date.parse("2013-12-27")
-weeknum    = date_stop.cweek # since we start on a saturday use then stop date
+mantid_src = ARGV.first
+if not File.exist?(mantid_src)
+  puts "Directory does not exist: \"#{mantid_src}\""
+  puts optparse.help()
+end
+date_start = options[:start]
+date_stop  = options[:start] + 6
+weeknum    = date_stop.cweek # since we start on a saturday use the stop date
 year       = date_stop.year
+filename   = options[:weekfile]
 
 # diagnostics
 #puts "Using git in '#{mantid_src}' from #{date_start} to #{date_stop}"
@@ -46,28 +86,37 @@ git_args << "--grep=\"Merge remote\" --branches=master --format=\"%H %h %s\""
 # run the command to get the relavant logs
 output = `cd #{mantid_src}; git log #{git_args}`
 
-# class for converting the information into a line in the output file
 class GitInfo
+  # class for converting the information into a line in the output file
+
   def initialize log_line
     log_line = log_line.split
     @sha1 = log_line[0]
     log_line.delete_at(0)
     @shashort = log_line[0]
     log_line.delete_at(0)
-    # get the ticket number in two steps to insure it is the right thing
+    # get the ticket number in two steps to
+    # insure it is the right thing
     @ticket = log_line.join(" ")[/(origin\S+\d+)/]
     @ticket = @ticket[/\d+/]
 
-    uri = URI("http://trac.mantidproject.org/mantid/ticket/#{@ticket}")
-    doc = Net::HTTP.get(uri)
-    summary = doc[/<h2 class=\"summary searchable\">.+<\/h2>/]
-    summary = summary.gsub(/<h2.+\">/, "")
-    @descr = summary.gsub(/<\/h2>/, "")
+  end
 
+  def <=> other
+    return @ticket <=> other.ticket
   end
 
   def to_s
     # override default to string method
+
+    if not @descr
+      uri = URI("http://trac.mantidproject.org/mantid/ticket/#{@ticket}")
+      doc = Net::HTTP.get(uri)
+      summary = doc[/<h2 class=\"summary searchable\">.+<\/h2>/]
+      summary = summary.gsub(/<h2.+\">/, "")
+      @descr = summary.gsub(/<\/h2>/, "")
+    end
+
     return "* {% include trac_detail trac=\"#{@ticket}\" sha1=\"#{@sha1}\" short=\"#{@shashort}\" descr=\"#{@descr}\" %}"
   end
 
@@ -76,6 +125,7 @@ class GitInfo
   end
 end
 
+# get the commits according to git
 commits = Array.new
 for line in output.split("\n")
   info = GitInfo.new line
@@ -83,6 +133,7 @@ for line in output.split("\n")
     commits << info
   end
 end
+commits = commits.sort
 
 # exit early if there aren't any merged tickets
 if commits.length == 0
