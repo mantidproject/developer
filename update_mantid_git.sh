e6c55914cea2ad67a3ec12522218b269665b917a
Pete Peterson <petersonpf@ornl.gov>
2014-05-02 15:20:43 -0400

    Debugging push section of the script.

--- update_mantid_git.sh
+++ update_mantid_git.sh
@@ -16,11 +16,11 @@ if [ -e _drafts/week*.md ] ; then
 fi
 tools/get_commit_details.rb mantid-code/
 tools/get_commit_details.rb mantid-systests/
+# right here should be something with: git status --porcelain
 if [ "$has_new_draft" = true ] ; then
     git add _drafts/week*.md
     git commit -m "Updating ticket list via automated script in cron"
 else
     git commit -a -m "Updating ticket list via automated script in cron"
 fi
-git push origin gh-pages
-cd -
+git push
