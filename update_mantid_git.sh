d2526f762b3a14f7df0afb8a2da8b097dc6a0355
Pete Peterson <petersonpf@ornl.gov>
2014-05-08 10:08:38 -0400

    Adding check for success on update

--- update_mantid_git.sh
+++ update_mantid_git.sh
@@ -15,8 +15,8 @@ has_new_draft=true
 if [ -e _drafts/week*.md ] ; then
     has_new_draft=false
 fi
-tools/get_commit_details.rb mantid-code/
-tools/get_commit_details.rb mantid-systests/
+tools/get_commit_details.rb mantid-code/ || return -1
+tools/get_commit_details.rb mantid-systests/ || return -1
 # right here should be something with: git status --porcelain
 if [ "$has_new_draft" = true ] ; then
     git add _drafts/week*.md
