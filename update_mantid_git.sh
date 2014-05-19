2af12d6372f2d4b6fb80e4e9242b950f81b4f8fa
Pete Peterson <petersonpf@ornl.gov>
2014-05-19 10:50:09 -0400

    Debugging builds.

--- update_mantid_git.sh
+++ update_mantid_git.sh
@@ -18,7 +18,8 @@ fi
 tools/get_commit_details.rb mantid-code/ || exit 1
 tools/get_commit_details.rb mantid-systests/ || exit 1
 # right here should be something with: git status --porcelain
-if [ `git status --porcelain` == "?? _drafts/" ] ; then
+git status --porcelain
+if [ `git status --porcelain` == "\?\? _drafts/" ] ; then
  echo "new file"
 fi
 if [ "$has_new_draft" = true ] ; then
