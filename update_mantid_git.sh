f3df9150705508e82e8b00c37ca4ed1aee01bda6
Pete Peterson <petersonpf@ornl.gov>
2014-05-19 10:43:22 -0400

    Debugging builds.

--- update_mantid_git.sh
+++ update_mantid_git.sh
@@ -18,7 +18,9 @@ fi
 tools/get_commit_details.rb mantid-code/ || exit 1
 tools/get_commit_details.rb mantid-systests/ || exit 1
 # right here should be something with: git status --porcelain
-git status --porcelain
+if [ $(git status --porcelain) == ??* ] ; then
+ echo "new file"
+fi
 if [ "$has_new_draft" = true ] ; then
     git add _drafts/week*.md
     git commit -m "Updating ticket list via jenkins"
