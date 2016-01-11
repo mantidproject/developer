35515fc32542c88c7cab6402cc37b0a9c277c0eb
Pete Peterson <petersonpf@ornl.gov>
2016-01-11 14:03:40 -0500

    Fixing link to skipped system tests

--- update_mantid_git.sh
+++ update_mantid_git.sh
@@ -12,7 +12,7 @@ git commit -m "Updating ticket list via jenkins"
 
 # update the skipped system test summary
 if [ ! -f skipped_systemtests.py ]; then
-    wget https://raw.githubusercontent.com/mantidproject/mantid/master/Code/Tools/skipped_systemtests.py
+    wget https://raw.githubusercontent.com/mantidproject/mantid/master/tools/skipped_systemtests.py
 fi
 python skipped_systemtests.py > systemtests/index.md
 git commit -m "Updating skipped system tests via jenkins" systemtests/index.md
